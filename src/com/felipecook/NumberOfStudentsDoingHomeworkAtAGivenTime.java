decf7c86ef65b9c8e609853c6c58b03ee6c28e33
Felipe Cook <felipepcook@gmail.com>
2020-05-23 09:49:49 -0600

    + NumberOfStudentsDoingHomeworkAtAGivenTime.java

---
+++ src/com/felipecook/NumberOfStudentsDoingHomeworkAtAGivenTime.java
@@ -0,0 +1,91 @@
+package com.felipecook;
+
+/*
+* NumberOfStudentsDoingHomeworkAtAGivenTime
+*
+* Given two integer arrays startTime and endTime and given an integer queryTime.
+
+The ith student started doing their homework at the time startTime[i]
+and finished it at time endTime[i].
+
+Return the number of students doing their homework at time queryTime.
+More formally, return the number of students where queryTime lays in
+the interval [startTime[i], endTime[i]] inclusive.
+
+
+
+Example 1:
+
+Input: startTime = [1,2,3], endTime = [3,2,7], queryTime = 4
+Output: 1
+Explanation: We have 3 students where:
+The first student started doing homework at time 1 and finished at time 3
+and wasn't doing anything at time 4.
+The second student started doing homework at time 2 and finished at time 2
+and also wasn't doing anything at time 4.
+The third student started doing homework at time 3 and finished at time 7
+and was the only student doing homework at time 4.
+
+Example 2:
+
+Input: startTime = [4], endTime = [4], queryTime = 4
+Output: 1
+Explanation: The only student was doing their homework at the queryTime.
+
+Example 3:
+
+Input: startTime = [4], endTime = [4], queryTime = 5
+Output: 0
+
+Example 4:
+
+Input: startTime = [1,1,1,1], endTime = [1,3,2,4], queryTime = 7
+Output: 0
+
+Example 5:
+
+Input: startTime = [9,8,7,6,5,4,3,2,1], endTime = [10,10,10,10,10,10,10,10,10], queryTime = 5
+Output: 5
+
+
+
+Constraints:
+
+    startTime.length == endTime.length
+    1 <= startTime.length <= 100
+    1 <= startTime[i] <= endTime[i] <= 1000
+    1 <= queryTime <= 1000
+
+
+*
+* */
+
+public class NumberOfStudentsDoingHomeworkAtAGivenTime {
+
+  public static int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+    int counter = 0;
+    for (int i = 0; i < startTime.length; i++) {
+      if (isBetween(startTime[i], endTime[i], queryTime)){
+        counter++;
+      }
+    }
+    return counter;
+  }
+
+  private static boolean isBetween(int startTime, int endTime, int queryTime){
+    for(int i = startTime; i <= endTime; i++){
+      if(i == queryTime){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static void main(String[] args) {
+    int[] startTime = new int[]{4};
+    int[] endTime = new int[]{4};
+    int queryTime = 5;
+    System.out.println(busyStudent(startTime, endTime, queryTime));
+  }
+
+}
